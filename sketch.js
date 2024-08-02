ca401c2bcd543477a3db69ea7d35378c2f386a0a
Thifany22 <00001118901848sp@al.educacao.sp.gov.br>
2024-08-01 22:56:02 -0300

    tentativa de correção de bug

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
 function setup() {
-    createCanvas(400, 250); background("blue");
-   
-    
-  }
+  createCanvas(400, 250); background("blue");
+ 
   
-  function draw() {
-   
-   
-    stroke ("black")
-    fill("red")
-    
-   // console.log(mouseIsPressed);
-    
-    if(mouseIsPressed) {
-    rect(mouseX, mouseY, 15, 15);
-    }
+}
+
+function draw() {
+ 
+ 
+  stroke ("black")
+  fill("red")
+  
+ // console.log(mouseIsPressed);
+  
+  if(mouseIsPressed) {
+  rect(mouseX, mouseY, 15, 15);
+  }
+}
