9bde55319d25ccdfec3e48c0c59b40ca26eed9bc
Thifany22 <00001118901848sp@al.educacao.sp.gov.br>
2024-08-01 22:28:40 -0300

    adicionando arquivo css e js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 250); background("blue");
+   
+    
+  }
+  
+  function draw() {
+   
+   
+    stroke ("black")
+    fill("red")
+    
+   // console.log(mouseIsPressed);
+    
+    if(mouseIsPressed) {
+    rect(mouseX, mouseY, 15, 15);
+    }
